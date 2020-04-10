6cdf345dfbf37141f433cc2f609dee51dccc54eb
Jeremy Gibson <jgibson@caktusgroup.com>
2020-04-10 10:22:26 -0400

    Adds logging. Moves Dockerfile

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -1,18 +1,23 @@
 import json
+import logging
 from channels.generic.websocket import WebsocketConsumer
+
+logger = logging.getLogger(__name__)
 
 class PointyHome(WebsocketConsumer):
     def connect(self):
+        logger.info("Socket connected")
         self.accept()
+        logger.info("Socket accepted")
 
     def disconnect(self, close_code):
-        pass
+        logger.info("Socket disconnected")
 
     def receive(self, text_data):
-
         text_data_json = json.loads(text_data)
+        logger.info(f"Socket received: {text_data_json}")
         message = text_data_json['message']
-
+        logger.info(f"Socket send: {message}")
         self.send(text_data=json.dumps({
             'message': message
         }))
