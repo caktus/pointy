1ca5370595d669b51014234b675b6f08ef0ad144
Jeremy Gibson <jgibson@caktusgroup.com>
2020-04-10 11:15:58 -0400

    Adds build for frontend

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,5 +1,5 @@
 // Socket config
 
-export const BASE_SOCKET_URL = 'ws://localhost:8000/ws'
+export const BASE_SOCKET_URL = `${process.env.REACT_APP_WS_HOST_ADDRESS}/ws`
 export const MAX_RECONNECT_ATTEMPTS = 1
 export const RECONNECT_ATTEMPT_INVERVAL = 100 // in ms
