b1b75d359e6490440082dd392d90adf7a857e734
Karen Tracey <kmtracey@caktusgroup.com>
2020-04-10 10:30:21 -0400

    Add 2nd consumer, do all the group stuff...
    ...so our messages will get published to all connected.

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -1,17 +1,49 @@
-import json
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
 
-class PointyHome(WebsocketConsumer):
+HOME_GROUP_NAME = "PointyHome"
+
+class PointyHome(JsonWebsocketConsumer):
     def connect(self):
+        async_to_sync(self.channel_layer.group_add)(
+            HOME_GROUP_NAME,
+            self.channel_name
+        )
         self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            HOME_GROUP_NAME,
+            self.channel_name
+        )
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+    def receive_json(self, content, **kwargs):
+        # Currently just echoes back what we get
+        async_to_sync(self.channel_layer.group_send)(
+            HOME_GROUP_NAME,
+            content
+        )
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+
+class PointySession(JsonWebsocketConsumer):
+    def connect(self):
+        self.session_id = self.scope["url_route"]["kwargs"]["session_id"]
+        self.group_name = f"pointy_{self.session_id}"
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name
+        )
+        self.accept()
+
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name,
+            self.channel_name
+        )
+
+    def receive_json(self, content, **kwargs):
+        # Currently just echoes back what we get
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            content
+        )
--- backend/core/routing.py
+++ backend/core/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/pointy/$', consumers.PointyHome),
+    re_path(r"ws/pointy/$", consumers.PointyHome),
+    re_path(r"ws/pointy/(?P<session_id>\w+)/$", consumers.PointySession),
 ]
