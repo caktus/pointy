5c0b1eec1d1fa31fc3fd2068f4fe010a99a67630
Colin Copeland <copelco@caktusgroup.com>
2020-05-05 12:32:17 -0400

    add production task

--- tasks.py
+++ tasks.py
@@ -8,19 +8,23 @@ init(autoreset=True)
 
 
 @invoke.task
+def production(c):
+    c.config.env = "production"
+    c.config.namespace = f"pointy-{c.config.env}"
+    with c.cd("frontend/"):
+        c.run('echo "REACT_APP_WS_HOST_ADDRESS=ws://pointy.caktus-built.com" > .env.production')
+
+
+@invoke.task
 def api(c):
-    c.config.env = "staging"
-    c.config.namespace = "pointy-staging"
-    c.config.build = "api"
-    c.config.app = "pointy_api"
+    c.config.service = "api"
+    c.config.app = f"pointy_{c.config.service}"
 
 
 @invoke.task
 def web(c):
-    c.config.env = "staging"
-    c.config.namespace = "pointy-staging"
-    c.config.build = "web"
-    c.config.app = "pointy_web"
+    c.config.service = "web"
+    c.config.app = f"pointy_{c.config.service}"
 
 
 ns = invoke.Collection()
@@ -28,6 +32,7 @@ ns.add_collection(image)
 ns.add_collection(aws)
 ns.add_collection(deploy)
 ns.add_collection(pod)
+ns.add_task(production)
 ns.add_task(api)
 ns.add_task(web)
 ns.configure({"run": {"echo": True}})
